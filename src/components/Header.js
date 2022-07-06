c94031d3862b34f24855137935b19a47132834b4
Jose Pascual <jmpascualjimenez@gmail.com>
2022-07-06 19:53:15 +0200

    feat: add border-radius top to inspirational layout design

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = ({ className = "" }) => {
 	return (
 		<header className="relative container mx-auto">
 			<div
-				className="absolute top-0 left-0 right-0 bottom-0 z-10 flex justify-center items-center text-white font-semibold tracking-wide"
+				className="absolute top-0 left-0 right-0 bottom-0 z-10 flex justify-center items-center text-white font-semibold tracking-wide rounded-t-lg"
 				style={{ background: "rgba(0,0,0,0.7)" }}
 			>
 				JUST A INSPIRATIONAL DESIGN FOR HEADER
